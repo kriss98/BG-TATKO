39653613f541f3a7a558ec296cbf8b53c0646cf1
Kristofar Hristov <hristov.kristofar@gmail.com>
2020-10-27 18:51:36 +0200

    Modified login and register views

--- src/BGTATKO.Common/GlobalConstants.cs
+++ src/BGTATKO.Common/GlobalConstants.cs
@@ -5,5 +5,7 @@
         public const string SystemName = "BGTATKO";
 
         public const string AdministratorRoleName = "Administrator";
+
+        public const string DefaultProfilePictureUrl = "http://ssl.gstatic.com/accounts/ui/avatar_2x.png";
     }
 }
