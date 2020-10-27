850f77f5cd54a1c803dc55b81eb17671a4d48167
Kristofar Hristov <hristov.kristofar@gmail.com>
2020-10-28 00:16:58 +0200

    Modified Layout view

--- src/BGTATKO.Common/GlobalConstants.cs
+++ src/BGTATKO.Common/GlobalConstants.cs
@@ -2,7 +2,7 @@
 {
     public static class GlobalConstants
     {
-        public const string SystemName = "BGTATKO";
+        public const string SystemName = "BG-TATKO";
 
         public const string AdministratorRoleName = "Administrator";
 
