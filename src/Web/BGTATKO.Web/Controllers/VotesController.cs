c3f34103403c38ac0e3ea8d8674d5a2805815e4b
Kristofar Hristov <hristov.kristofar@gmail.com>
2020-10-29 01:57:01 +0200

    Implement voting controllers

--- src/Web/BGTATKO.Web/Controllers/VotesController.cs
+++ src/Web/BGTATKO.Web/Controllers/VotesController.cs
@@ -1,11 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace BGTATKO.Web.Controllers
+﻿namespace BGTATKO.Web.Controllers
 {
-    public class VotesController
+    using System.Threading.Tasks;
+    using Data.Models;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Mvc;
+
+    [ApiController]
+    [Route("api/{controller}")]
+    public class VotesController : ControllerBase
     {
+        private readonly IVotesService votesService;
+
+        private readonly UserManager<ApplicationUser> userManager;
+
+        public VotesController(IVotesService votesService, UserManager<ApplicationUser> userManager)
+        {
+            this.votesService = votesService;
+            this.userManager = userManager;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<VoteResponseModel>> Post(VoteInputModel input)
+        {
+            var user = await this.userManager.GetUserAsync(this.User);
+            await this.votesService.VoteAsync(input.PostId, user.Id, input.IsUpVote);
+            var votes = this.votesService.GetVotes(input.PostId);
+
+            return new VoteResponseModel
+            {
+                VotesCount = votes,
+            };
+        }
+
+        [HttpGet("{postId}")]
+        public async Task<ActionResult<int>> Get(int postId)
+        {
+            var user = await this.userManager.GetUserAsync(this.User);
+            var voteStatus = await this.votesService.GetVoteStatus(postId, user.Id);
+
+            return voteStatus;
+        }
     }
 }
--- src/Web/BGTATKO.Web/Controllers/VotesForCommentsController.cs
+++ src/Web/BGTATKO.Web/Controllers/VotesForCommentsController.cs
@@ -1,11 +1,47 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace BGTATKO.Web.Controllers
+﻿namespace BGTATKO.Web.Controllers
 {
-    public class VotesForCommentsControllet
+    using System.Threading.Tasks;
+    using Data.Models;
+    using Microsoft.AspNetCore.Authorization;
+    using Microsoft.AspNetCore.Identity;
+    using Microsoft.AspNetCore.Mvc;
+
+    [ApiController]
+    [Route("api/{controller}")]
+    public class VotesForCommentsController : ControllerBase
     {
+        private readonly IVotesForCommentsService votesForCommentsService;
+
+        private readonly UserManager<ApplicationUser> userManager;
+
+        public VotesForCommentsController(IVotesForCommentsService votesForCommentsService, UserManager<ApplicationUser> userManager)
+        {
+            this.votesForCommentsService = votesForCommentsService;
+            this.userManager = userManager;
+        }
+
+        [HttpPost]
+        [Authorize]
+        public async Task<ActionResult<VoteForCommentResponseModel>> Post(VoteForCommentInputModel inputModel)
+        {
+            var user = await this.userManager.GetUserAsync(this.User);
+            await this.votesForCommentsService.VoteAsync(inputModel.CommentId, user.Id, inputModel.IsUpVote);
+            var votes = this.votesForCommentsService.GetVotes(inputModel.CommentId);
+
+            return new VoteForCommentResponseModel
+            {
+                VotesCount = votes,
+            };
+        }
+
+        [HttpGet("{commentId}")]
+        [Authorize]
+        public async Task<ActionResult<int>> Get(int commentId)
+        {
+            var user = await this.userManager.GetUserAsync(this.User);
+            var voteStatus = await this.votesForCommentsService.GetVoteStatus(commentId, user.Id);
+
+            return voteStatus;
+        }
     }
 }
